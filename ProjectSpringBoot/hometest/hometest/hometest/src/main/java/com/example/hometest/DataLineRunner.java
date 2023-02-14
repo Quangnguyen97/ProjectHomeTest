b44bfedaed33938c2004dba1f2c623d5ca12aa6b
Nguyễn Huy Quang <nhq14081997@gmail.com>
2023-02-14 11:52:25 +0700

    .

---
+++ ProjectSpringBoot/hometest/hometest/hometest/src/main/java/com/example/hometest/DataLineRunner.java
@@ -0,0 +1,33 @@
+package com.example.hometest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import com.example.hometest.User.*;
+import com.example.hometest.Account.*;
+
+@Component
+public class DataLineRunner implements CommandLineRunner {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private AccountRepository accountRepository;
+
+    @Override
+    public void run(String... args) throws Exception {
+        try {
+            userRepository.save(new User(1, "NGUYEN VAN A", "hash1", "token1"));
+            userRepository.save(new User(2, "NGUYEN VAN B", "hash2", "token2"));
+            userRepository.save(new User(3, "NGUYEN VAN C", "hash3", "token3"));
+
+            accountRepository.save(new Account(1, 10, 100));
+            accountRepository.save(new Account(2, 20, 200));
+            accountRepository.save(new Account(3, 30, 300));
+        } catch (Exception e) {
+            new Exception(e);
+        }
+    }
+}
