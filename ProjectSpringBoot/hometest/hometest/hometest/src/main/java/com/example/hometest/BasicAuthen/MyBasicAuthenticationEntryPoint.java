1ab7ddd957639b14734ba21d7fcd0fe8e8b79a3c
Nguyễn Huy Quang <nhq14081997@gmail.com>
2023-02-15 13:40:36 +0700

    .

--- ProjectSpringBoot/hometest/hometest/hometest/src/main/java/com/example/hometest/BasicAuthen/MyBasicAuthenticationEntryPoint.java
+++ ProjectSpringBoot/hometest/hometest/hometest/src/main/java/com/example/hometest/BasicAuthen/MyBasicAuthenticationEntryPoint.java
@@ -8,8 +8,6 @@ import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.www.BasicAuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
@@ -26,19 +24,10 @@ public class MyBasicAuthenticationEntryPoint extends BasicAuthenticationEntryPoi
     public void commence(HttpServletRequest request,
             HttpServletResponse response, AuthenticationException authException)
             throws IOException {
-        Map<String, Object> map = new HashMap<>();
-        map.put("code", HttpServletResponse.SC_UNAUTHORIZED);
-        map.put("msg", authException.getMessage());
-        map.put("path", request.getServletPath());
-        map.put("timestamp", System.currentTimeMillis());
         response.addHeader("WWW-Authenticate", "Basic realm=" + getRealmName());
+        response.setContentType("application/json");
         response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
-        response.getWriter().println("HTTP Status 401 - " + authException.getMessage());
-        try {
-            ObjectMapper mapper = new ObjectMapper();
-            mapper.writeValue(response.getOutputStream(), map);
-        } catch (Exception e) {
-            throw new IOException();
-        }
+        PrintWriter writer = response.getWriter();
+        writer.println("{\"status\":" + SC_UNAUTHORIZED + ", \"message\":\"" + authException.getMessage() + "\"}");
     }
 }
